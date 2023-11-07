28b10691723f32110f05b6263b7631e8609c2800
Mostafa Mahdijoo <mostafa.mahdijoo@gmail.com>
2023-11-07 16:56:47 -0400

    spring environment properties

---
+++ src/test/java/guru/springframework/test/external/props/PropertySourceEnvTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.test.external.props;
+
+import guru.springframework.test.jms.FakeJmsBroker;
+import guru.test.config.external.props.ExternalPropsEnvironment;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ExternalPropsEnvironment.class)
+public class PropertySourceEnvTest {
+
+    @Autowired
+    FakeJmsBroker fakeJmsBroker;
+
+    @Test
+    public void testPorpsSet() {
+        assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
+        assertEquals(3330, fakeJmsBroker.getPort().intValue());
+        assertEquals("Ron", fakeJmsBroker.getUser());
+        assertEquals("Burgundy", fakeJmsBroker.getPassword());
+    }
+
+}
