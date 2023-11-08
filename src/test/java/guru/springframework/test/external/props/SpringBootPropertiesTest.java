fd0b06a9b7b6c9904117858a6be14ae285a075e9
Mostafa Mahdijoo <mostafa.mahdijoo@gmail.com>
2023-11-07 20:35:54 -0400

    spring boot application properties

---
+++ src/test/java/guru/springframework/test/external/props/SpringBootPropertiesTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.test.external.props;
+
+import guru.springframework.SpringCoreDevOpsApplication;
+import guru.springframework.test.jms.FakeJmsBroker;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(SpringCoreDevOpsApplication.class)
+@WebIntegrationTest
+@TestPropertySource("/application.properties")
+public class SpringBootPropertiesTest {
+
+    @Autowired
+    FakeJmsBroker fakeJmsBroker;
+
+    @Test
+    public void testPropsSet() {
+        assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
+        assertEquals(3330, fakeJmsBroker.getPort().intValue());
+        assertEquals("Ron", fakeJmsBroker.getUser());
+        assertEquals("Burgundy", fakeJmsBroker.getPassword());
+    }
+
+}
