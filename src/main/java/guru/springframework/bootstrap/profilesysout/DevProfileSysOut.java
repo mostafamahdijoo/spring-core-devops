7109510acd82da6b6e114fe970c60a1fc40c4e9c
Mostafa Mahdijoo <mostafa.mahdijoo@gmail.com>
2023-11-08 14:42:41 -0400

    springboot profile properties assignment

--- src/main/java/guru/springframework/bootstrap/profilesysout/DevProfileSysOut.java
+++ src/main/java/guru/springframework/bootstrap/profilesysout/DevProfileSysOut.java
@@ -1,15 +1,19 @@
 package guru.springframework.bootstrap.profilesysout;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("dev")
 public class DevProfileSysOut {
-    public DevProfileSysOut() {
+    @Autowired
+    public DevProfileSysOut(@Value("${guru.springframework.profile.message}") String msg) {
         System.out.println("##############################");
         System.out.println("##############################");
         System.out.println("###           Dev          ###");
+        System.out.println(msg);
         System.out.println("##############################");
         System.out.println("##############################");
     }
