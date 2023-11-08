7fdb7d4a08c3c18c98139fcb3274e5e734a781b5
Mostafa Mahdijoo <mostafa.mahdijoo@gmail.com>
2023-11-08 13:10:45 -0400

    springboot profile properties

--- src/main/java/guru/springframework/bootstrap/profilesysout/DefaultProfileSysOut.java
+++ src/main/java/guru/springframework/bootstrap/profilesysout/DefaultProfileSysOut.java
@@ -1,15 +1,19 @@
 package guru.springframework.bootstrap.profilesysout;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("default")
 public class DefaultProfileSysOut {
-    public DefaultProfileSysOut() {
+    @Autowired
+    public DefaultProfileSysOut(@Value("${guru.springframework.profile.message}") String msg) {
         System.out.println("##############################");
         System.out.println("##############################");
         System.out.println("###         DEFAULT        ###");
+        System.out.println(msg);
         System.out.println("##############################");
         System.out.println("##############################");
     }
