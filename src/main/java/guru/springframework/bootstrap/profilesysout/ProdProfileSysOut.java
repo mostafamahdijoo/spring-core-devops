84ede84e3a71333a1f1d7efc6c5305b38a65e788
Mostafa Mahdijoo <mostafa.mahdijoo@gmail.com>
2023-11-08 01:18:27 -0400

    setting the active profile at runtime

---
+++ src/main/java/guru/springframework/bootstrap/profilesysout/ProdProfileSysOut.java
@@ -0,0 +1,16 @@
+package guru.springframework.bootstrap.profilesysout;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("prod")
+public class ProdProfileSysOut {
+    public ProdProfileSysOut() {
+        System.out.println("##############################");
+        System.out.println("##############################");
+        System.out.println("###          PROD          ###");
+        System.out.println("##############################");
+        System.out.println("##############################");
+    }
+}
