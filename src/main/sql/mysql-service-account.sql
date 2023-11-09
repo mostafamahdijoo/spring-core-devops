bc0fbbb8a36aea055e8167cd300bfd77668cc2bb
Mostafa Mahdijoo <mostafa.mahdijoo@gmail.com>
2023-11-08 22:10:43 -0400

    mysql assignment: adding qa2 profile

--- src/main/sql/mysql-service-account.sql
+++ src/main/sql/mysql-service-account.sql
@@ -4,3 +4,11 @@ GRANT SELECT ON springguru.* to 'springframework'@'localhost';
 GRANT INSERT ON springguru.* to 'springframework'@'localhost';
 GRANT DELETE ON springguru.* to 'springframework'@'localhost';
 GRANT UPDATE ON springguru.* to 'springframework'@'localhost';
+
+
+CREATE USER 'qa2'@'localhost' IDENTIFIED BY 'guru';
+
+GRANT SELECT ON qa2.* to 'qa2'@'localhost';
+GRANT INSERT ON qa2.* to 'qa2'@'localhost';
+GRANT DELETE ON qa2.* to 'qa2'@'localhost';
+GRANT UPDATE ON qa2.* to 'qa2'@'localhost';
